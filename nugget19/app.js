ddb9e9fa2c901f79167775138d15ee3ba5dde4a8
= <atharvarsalokhe2@gmail.com>
2021-12-16 11:02:00 +0530

    fetch errors

--- nugget19/app.js
+++ nugget19/app.js
@@ -1,1 +1,28 @@
 // fetch errors:
+const url = "https://www.course-api.com/react-tours-projects";
+
+// let's fetch url data:
+// fetch throws an error only when there are network errors
+// while fetching requests!!
+
+const fetchData = async() =>{
+    try {
+        const res = await fetch(url);
+        if(!res.ok){
+            const msg = `Error while fetching ${res.status} ${res.statusText}`
+            throw new Error(msg);
+        }
+        const data = await res.json();
+        console.log(data.map((obj) => obj.name))
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+const btn = document.querySelector('.btn');
+
+
+// remember to pass in instances instead of invoking func while passing
+// in as arguments
+btn.addEventListener("click", fetchData);
